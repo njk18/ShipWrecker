52348c75d26fb74dc22ff9b149176892af2bd0f2
Nicolas <nicolas-kik@nicolas-kik.com>
2020-03-29 21:07:08 +0300

    Added a function to block a player from firing till the other player has fired. Also allows to notify player of his turn.

---
+++ WaitTurn.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ShipWrecker
+{
+    public static class WaitTurn
+    {
+        [FunctionName("WaitTurn")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Block till it is the requestin player's turn.");
+
+            Board.playerType playerType = (Board.playerType)System.Enum.Parse(typeof(Board.playerType), req.Query["playerType"]);
+
+            // Block till the previousTurn changes
+            while (FireResponse.previousTurn == playerType) { };
+
+            return new OkObjectResult(true);
+        }
+    }
+}
