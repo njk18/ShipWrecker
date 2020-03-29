74b4cda950a99f82e39abb8f5d0630ad142dd493
Nicolas <nicolas-kik@nicolas-kik.com>
2020-03-29 17:58:10 +0300

    Use previousTurn

--- Fire.cs
+++ Fire.cs
@@ -28,7 +28,7 @@ namespace ShipWrecker
             
             Board currentBoard = null;
 
-            if (response.playerTurn != Enum.GetName(typeof(Board.playerType), playerType) || AddShip.countPlayerOneShipAdded != 5 || AddShip.countPlayerTwoShipAdded != 5) {
+            if (FireResponse.previousTurn != playerType || AddShip.countPlayerOneShipAdded != 5 || AddShip.countPlayerTwoShipAdded != 5) {
                 // Return a JSON response
                 var wrongPlayerResponse = JsonConvert.SerializeObject(response, Formatting.Indented);
                 return (ActionResult)new OkObjectResult(wrongPlayerResponse);
